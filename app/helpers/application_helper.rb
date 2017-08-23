0feca5c01ca8c0d576d4d1f975ad877a8f5b22d1
Chdinma1990 <nonsowinner@gmail.com>
2017-08-23 17:06:41 -0500

    initial commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,10 @@ require 'date'
 module ApplicationHelper
 
 	def date_of_next(day)
-	  date  = Date.parse(day)
-	  delta = date >= Date.today ? 0 : 7
-	  date + delta
-	end
+    date  = Date.parse(day)
+    delta = date >= Date.today ? 0 : 7
+    date + delta
+  end
 
   
 
