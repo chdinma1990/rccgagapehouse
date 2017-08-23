a0d7013b77d8bb8fd6edf6eb16befe7e623036aa
Chdinma1990 <nonsowinner@gmail.com>
2017-08-22 20:24:03 -0500

    initial commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,33 @@
+require 'date'
+
+
 module ApplicationHelper
+
+	def date_of_next(day)
+	  date  = Date.parse(day)
+	  delta = date >= Date.today ? 0 : 7
+	  date + delta
+	end
+
+  
+
+  def bootstrap_class_for flash_type
+    { success: 'alert-success', error: 'alert-danger', alert: 'alert-warning', warning: 'alert-warning', notice: 'alert-info'}[flash_type.to_sym]
+  end
+
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "text-center alert #{bootstrap_class_for(msg_type)} alert-dismissible", role: 'alert') do
+        concat(content_tag(:button, class: 'close', data: { dismiss: 'alert' }) do
+          concat content_tag(:span, '&times;'.html_safe, 'aria-hidden' => true)
+          concat content_tag(:span, 'Close', class: 'sr-only')
+        end)
+        concat message
+      end)
+    end
+    nil
+  end
+
+  
 end
+
