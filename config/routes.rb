c9446b24ca772ab45da9a43ce94648623918bd34
Chdinma1990 <nonsowinner@gmail.com>
2017-08-25 09:43:18 -0500

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
-  
+  root   'home#index'
 
   get 'about' => 'about#us'
   get 'staff' => 'staff#staff'
   get 'contact' => 'contact#contact'
+  get 'media'  => 'media#Services'
+  get 'index' => 'home#index'
 
 
-  #get 'home/index'
  
    get 'donate' => 'donate#new'
+  
+  
 
-  root 'home#index'
+ 
  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
